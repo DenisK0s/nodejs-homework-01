b40f128c503e29e8f3eac719d3f0225b4a0e9ca3
DenisK0s <kostenkoden89@gmail.com>
2022-01-16 18:24:06 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+add - https://ibb.co/NS3M54x
+get - https://ibb.co/sQQXYDN
+list - https://ibb.co/4W2b8dK
+remove - https://ibb.co/5hyDmgw
---
+++ db/contacts.js
@@ -0,0 +1,44 @@
+const { readFile, writeFile } = require("fs").promises;
+const path = require("path");
+const { v4 } = require("uuid");
+
+const contactsPath = path.join(__dirname, "contacts.json");
+
+const isContactExist = (contact) => contact ?? null;
+
+const normaliseId = (id) => (isNaN(id) ? id : id + "");
+
+async function listContacts() {
+  const contacts = await readFile(contactsPath);
+
+  return JSON.parse(contacts);
+}
+
+async function getContactById(contactId) {
+  const contacts = await listContacts();
+  const normalisedId = normaliseId(contactId);
+  const contactById = contacts.find((contact) => contact.id === normalisedId);
+
+  return isContactExist(contactById);
+}
+
+async function addContact({ name, email, phone }) {
+  const contacts = await listContacts();
+  const newContact = { id: v4(), name, email, phone };
+  contacts.push(newContact);
+  await writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+
+  return newContact;
+}
+
+async function removeContact(contactId) {
+  const contacts = await listContacts();
+  const normalisedId = normaliseId(contactId);
+  const removedContact = contacts.find((contact) => contact.id === normalisedId);
+  const newContacts = contacts.filter((contact) => contact.id !== normalisedId);
+  await writeFile(contactsPath, JSON.stringify(newContacts, null, 2));
+
+  return isContactExist(removedContact);
+}
+
+module.exports = { listContacts, getContactById, removeContact, addContact };
